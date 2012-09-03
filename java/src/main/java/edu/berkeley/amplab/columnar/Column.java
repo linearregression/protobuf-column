0969cf9bd2cd2c318ed626182300c4e0ee87a44b
Reynold Xin <rxin@cs.berkeley.edu>
2012-09-03 00:12:20 -0700

    Added ColumnarCollection and various primitive Column classes.

---
+++ java/src/main/java/edu/berkeley/amplab/columnar/Column.java
@@ -0,0 +1,253 @@
+package edu.berkeley.amplab.columnar;
+
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+
+import it.unimi.dsi.fastutil.bytes.ByteArrayList;
+import it.unimi.dsi.fastutil.doubles.DoubleArrayList;
+import it.unimi.dsi.fastutil.floats.FloatArrayList;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.longs.LongArrayList;
+
+/**
+ * A collection of basic columns.
+ */
+public abstract class Column {
+
+  /**
+   * Initialize a new column for use.
+   * 
+   * @param initialCapacity
+   *          The initial capacity to allocate.
+   */
+  public abstract void initialize(int initialCapacity);
+
+  /**
+   * Reconstruct a column from the binary data in the byte buffer.
+   * 
+   * @param byteBuffers
+   */
+  public abstract void initialize(Iterator<ByteBuffer> byteBuffers);
+
+  /**
+   * Primitive integer column.
+   */
+  public static class IntColumn extends Column {
+    private IntArrayList arr;
+
+    @Override
+    public void initialize(int initialCapacity) {
+      arr = new IntArrayList(initialCapacity);
+    }
+
+    @Override
+    public void initialize(Iterator<ByteBuffer> byteBuffers) {
+      arr = new IntArrayList(byteBuffers.next().asIntBuffer().array());
+    }
+
+    public int get(int index) {
+      return arr.get(index);
+    }
+
+    public void add(int v) {
+      arr.add(v);
+    }
+  }
+
+  /**
+   * Primitive long column.
+   */
+  public static class LongColumn extends Column {
+    private LongArrayList arr;
+
+    @Override
+    public void initialize(int initialCapacity) {
+      arr = new LongArrayList(initialCapacity);
+    }
+
+    @Override
+    public void initialize(Iterator<ByteBuffer> byteBuffers) {
+      arr = new LongArrayList(byteBuffers.next().asLongBuffer().array());
+    }
+
+    public long get(int index) {
+      return arr.get(index);
+    }
+
+    public void add(long v) {
+      arr.add(v);
+    }
+  }
+
+  /**
+   * Primitive float column.
+   */
+  public static class FloatColumn extends Column {
+    private FloatArrayList arr;
+
+    @Override
+    public void initialize(int initialCapacity) {
+      arr = new FloatArrayList(initialCapacity);
+    }
+
+    @Override
+    public void initialize(Iterator<ByteBuffer> byteBuffers) {
+      arr = new FloatArrayList(byteBuffers.next().asFloatBuffer().array());
+    }
+
+    public float get(int index) {
+      return arr.get(index);
+    }
+
+    public void add(float v) {
+      arr.add(v);
+    }
+  }
+
+  /**
+   * Primitive double column.
+   */
+  public static class DoubleColumn extends Column {
+    private DoubleArrayList arr;
+
+    @Override
+    public void initialize(int initialCapacity) {
+      arr = new DoubleArrayList(initialCapacity);
+    }
+
+    @Override
+    public void initialize(Iterator<ByteBuffer> byteBuffers) {
+      arr = new DoubleArrayList(byteBuffers.next().asDoubleBuffer().array());
+    }
+
+    public double get(int index) {
+      return arr.get(index);
+    }
+
+    public void add(double v) {
+      arr.add(v);
+    }
+  }
+
+  /**
+   * Primitive byte column.
+   */
+  public static class ByteColumn extends Column {
+    private ByteArrayList arr;
+
+    @Override
+    public void initialize(int initialCapacity) {
+      arr = new ByteArrayList(initialCapacity);
+    }
+
+    @Override
+    public void initialize(Iterator<ByteBuffer> byteBuffers) {
+      arr = new ByteArrayList(byteBuffers.next().array());
+    }
+
+    public byte get(int index) {
+      return arr.get(index);
+    }
+
+    public void add(byte v) {
+      arr.add(v);
+    }
+  }
+
+  /**
+   * Primitive boolean column: backed by a byte array, where each byte is one
+   * boolean value.
+   */
+  public static class BooleanColumn extends Column {
+    private ByteArrayList arr;
+
+    @Override
+    public void initialize(int initialCapacity) {
+      arr = new ByteArrayList(initialCapacity);
+    }
+
+    @Override
+    public void initialize(Iterator<ByteBuffer> byteBuffers) {
+      arr = new ByteArrayList(byteBuffers.next().array());
+    }
+
+    public boolean get(int index) {
+      return arr.get(index) != 0;
+    }
+
+    public void add(boolean v) {
+      arr.add((byte) (v ? 1 : 0));
+    }
+  }
+
+  /**
+   * Primitive boolean column: backed by a long array, where 32 boolean values
+   * are packed into a single long.
+   */
+  public static class BooleanCompactColumn extends Column {
+
+    private static final int WORD_LENS_IN_BITS = Long.SIZE;
+    private LongArrayList arr;
+    int size;
+
+    @Override
+    public void initialize(int initialCapacity) {
+      arr = new LongArrayList(initialCapacity / WORD_LENS_IN_BITS);
+    }
+
+    @Override
+    public void initialize(Iterator<ByteBuffer> byteBuffers) {
+      arr = new LongArrayList(byteBuffers.next().asLongBuffer().array());
+    }
+
+    public boolean get(int index) {
+      return (arr.get(index / WORD_LENS_IN_BITS) & (1L << index)) != 0;
+    }
+
+    public void add(boolean v) {
+      if (size % WORD_LENS_IN_BITS == 0) {
+        arr.add(v ? 1L : 0L);
+      } else {
+        long[] elements = arr.elements();
+        elements[elements.length - 1] |= 1L << size;
+      }
+      size++;
+    }
+  }
+
+  /**
+   * String column: backed by a single byte array with a primitive integer array
+   * for position indexing.
+   */
+  public static class StringColumn extends Column {
+
+    private ByteArrayList arr;
+    private IntArrayList posIndex;
+
+    @Override
+    public void initialize(int initialCapacity) {
+      arr = new ByteArrayList(initialCapacity);
+      posIndex = new IntArrayList(initialCapacity + 1);
+      posIndex.add(0);
+    }
+
+    @Override
+    public void initialize(Iterator<ByteBuffer> byteBuffers) {
+      arr = new ByteArrayList(byteBuffers.next().array());
+      posIndex = new IntArrayList(byteBuffers.next().asIntBuffer().array());
+    }
+
+    public String get(int index) {
+      int start = posIndex.getInt(index);
+      return new String(arr.elements(), start, posIndex.getInt(index + 1)
+          - start);
+    }
+
+    public void add(String s) {
+      posIndex.add(arr.size() + s.length());
+      byte[] bytes = s.getBytes();
+      arr.addElements(arr.size(), bytes);
+    }
+  }
+
+}
---
+++ java/src/main/java/edu/berkeley/amplab/columnar/ColumnarCollection.java
@@ -0,0 +1,92 @@
+package edu.berkeley.amplab.columnar;
+
+import java.lang.Iterable;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public abstract class ColumnarCollection<E> implements Iterable<E> {
+
+  /**
+   * Create a new columnar collection.
+   * 
+   * @param initialCapacity
+   */
+  public abstract void initialize(int initialCapacity);
+
+  /**
+   * Create a columnar collection using the binary data from the list of
+   * ByteBuffers. This is the counterpart of asByteBuffers().
+   * 
+   * @param byteBuffers
+   *          The byte buffers to restore the collection from.
+   */
+  public abstract void initialize(Iterable<ByteBuffer> byteBuffers);
+
+  /**
+   * Serialize the collection into an array of ByteBuffer.
+   * 
+   * @return an array of ByteBuffer serializing this collection.
+   */
+  public abstract ByteBuffer[] asByteBuffers();
+
+  /**
+   * Returns the number of elements in this collection.
+   * 
+   * @return the number of elements in this collection.
+   */
+  public abstract int size();
+
+  /**
+   * Returns the element at the specified position in this collection.
+   * 
+   * @param index
+   *          index of the element to return
+   * @return the element at the specified position in this list of type
+   *         E.Columnar.
+   */
+  public abstract E get(int index);
+
+  /**
+   * Appends the specified element to the end of this collection.
+   * 
+   * @param elem
+   *          element to be appended to this collection.
+   */
+  public abstract void add(E elem);
+
+  /**
+   * Return an iterator for the elements in this columnar collection. The
+   * elements returned by the iterator should be of type E.Columnar.
+   */
+  public Iterator<E> iterator() {
+    return new Itr();
+  }
+
+  /**
+   * An iterator over the ColumnarCollection.
+   */
+  private class Itr implements java.util.Iterator<E> {
+
+    int lastRet = -1; // index of last element returned; -1 if no such
+    int cursor; // index of next element to return
+
+    public boolean hasNext() {
+      return cursor != size();
+    }
+
+    public E next() {
+      int i = cursor;
+      if (i >= size()) {
+        throw new NoSuchElementException();
+      }
+      cursor = i + 1;
+      lastRet = i;
+      return get(lastRet);
+    }
+
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+  }
+}
